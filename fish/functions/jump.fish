9586a3f4808105e11774891fe4138ca4c03374be
Jean Augusto Silva <jeanaugustoos@gmail.com>
2018-04-14 17:22:46 -0300

    functions: update jump

--- fish/functions/jump.fish
+++ fish/functions/jump.fish
@@ -1,8 +1,9 @@
 function jump
-	set project $argv[1]
-    set dir ~/pagarme/$project
+    set project $argv[1]
+    set dir ~/hipsterspace/$project
 
     if not test -d $dir
+        mkdir $dir
         git clone git@github.com:$project $dir
     end
 
